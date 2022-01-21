685d0547b31fd87581939732b3c7f8f48b808f35
Mohamad Hanafi <78841380+MohamadHanafi@users.noreply.github.com>
2022-01-21 12:32:17 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-import path from 'path';
-import express from 'express';
-import dotenv from 'dotenv';
-import colors from 'colors';
-import morgan from 'morgan';
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import morgan from "morgan";
 
-import connectDB from './config/db.js';
+import connectDB from "./config/db.js";
 
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -19,29 +19,31 @@ connectDB();
 
 const app = express();
 
-if (process.env.NODE_MODE === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_MODE === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(express.json());
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/upload', uploadRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
-if (process.env.NODE_MODE === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+console.log(process.env.NODE_MODE);
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+if (process.env.NODE_MODE === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running ...');
+  app.get("/", (req, res) => {
+    res.send("API is running ...");
   });
 }
 
